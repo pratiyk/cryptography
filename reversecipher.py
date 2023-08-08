caae8d02d7236b66564485b01f98e1ca94f0ab2c
Prateek <pratiyk.shrivastava@gmail.com>
2023-08-08 12:47:46 +0530

    new feature files added

---
+++ reversecipher.py
@@ -0,0 +1,8 @@
+message = input('Enter your message:' )
+translated = '' 
+i = len(message) - 1
+
+while i >= 0:
+   translated = translated + message[i]
+   i = i - 1
+print('The cipher text is :' , translated)
